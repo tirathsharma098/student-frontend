0d2057f5e06c7a0c87e3c62972ade0018230a991
Tirath Sharma <tirathsharma098@gmail.com>
2022-10-31 15:13:33 +0530

    added search form

--- src/Components/Student/SearchAndPageForm.jsx
+++ src/Components/Student/SearchAndPageForm.jsx
@@ -1,11 +1,25 @@
-import style from './SearchAndPageForm.module.css';
+import { Form, Row, Col } from "react-bootstrap";
+import style from "./SearchAndPageForm.module.css";
 
-const SearchAndPageForm = props => {
-    return(
-        <div>
-            
-        </div>
-    );
-}
+const SearchAndPageForm = (props) => {
+  return (
+    <div className={style["search-entries-form"]}>
+      <div className={style["display-total-entries"]}>
+        <div style={{"padding-right": "5px"}}>Show </div>
+        <Form.Select>
+          <option>20</option>
+          <option value="10">10</option>
+          <option value="20">20</option>
+          <option value="30">30</option>
+        </Form.Select>
+        <div> Entries</div>
+      </div>
+      <div className={style["search-student-container"]}>
+        <div className={style["search-text"]}> Search: </div>
+       <Form.Control className={style["search-control"]} type="text" name="search" />
+      </div>
+    </div>
+  );
+};
 
 export default SearchAndPageForm;
